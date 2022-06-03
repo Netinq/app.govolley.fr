4ba81536492be1e01f364ab61754899a84d16f63
BuildTools <sarquentin@gmail.com>
2022-06-03 02:38:11 +0200

    :wrench: Adding new route

--- types.tsx
+++ types.tsx
@@ -26,6 +26,7 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 
 export type RootTabParamList = {
   Terrains: undefined;
+  TerrainPage: undefined;
   Ajouter: undefined;
   Tournois: undefined;
 };
