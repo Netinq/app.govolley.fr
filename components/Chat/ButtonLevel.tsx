b3d49d605e76a118cdcb098fedd5050b161f25d9
BuildTools <sarquentin@gmail.com>
2022-06-06 03:08:12 +0200

    :sparkles: Adding new component

---
+++ components/Chat/ButtonLevel.tsx
@@ -0,0 +1,55 @@
+import { FontAwesome5 } from "@expo/vector-icons";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { CompositeNavigationProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Component } from "react";
+import { GestureResponderEvent, StyleSheet, Text, TouchableOpacity, TouchableOpacityBase, ViewStyle } from "react-native";
+import Layout from "../../constants/Layout";
+import { QuizzTabParamList, RootStackParamList } from "../../types";
+
+type Props = {
+  text: string,
+  subText: string,
+  color: string,
+  onPress: (event: GestureResponderEvent) => void,
+  style?: ViewStyle,
+}
+
+export default class ButtonLevel extends Component<Props> {
+
+  render() {
+
+    return (
+      <TouchableOpacity style={[this.styles.button, this.props.style]} onPress={this.props.onPress}>
+        <Text style={this.styles.text}>{this.props.text}</Text>
+        <Text style={this.styles.subText}>{this.props.subText}</Text>
+      </TouchableOpacity>
+    )
+
+  }
+
+  styles = StyleSheet.create({
+    button: {
+      flexDirection: "column",
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 10,
+      backgroundColor: this.props.color,
+      borderRadius: 15,
+      borderTopRightRadius: 0,
+      width: '45%',
+      marginBottom: ((Layout.window.width - 70)*10/100)
+    },
+    text: {
+      color: '#fff',
+      fontFamily: 'franklin-gothic',
+      fontSize: 20
+    },
+    subText: {
+      color: '#fff',
+      fontFamily: 'franklin-gothic',
+      fontSize: 15
+    }
+  })
+
+}
