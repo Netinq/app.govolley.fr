8f183956a7bfa4a3dd7b5b36941b017819762b07
BuildTools <sarquentin@gmail.com>
2022-06-06 02:43:54 +0200

    :sparkles: Adding touchable button

--- components/Chat/Button.tsx
+++ components/Chat/Button.tsx
@@ -3,24 +3,24 @@ import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { CompositeNavigationProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Component } from "react";
-import { StyleSheet, Text, TouchableOpacity, TouchableOpacityBase } from "react-native";
+import { GestureResponderEvent, StyleSheet, Text, TouchableOpacity, TouchableOpacityBase } from "react-native";
 import { QuizzTabParamList, RootStackParamList } from "../../types";
 
 type Props = {
   text: string,
-  navigation: CompositeNavigationProp<BottomTabNavigationProp<QuizzTabParamList>, NativeStackNavigationProp<RootStackParamList>>;
-  goTo: keyof RootStackParamList,
-  screen: keyof QuizzTabParamList,
+  onPress: (event: GestureResponderEvent) => void,
+  disable?: boolean;
 }
 
 export default class Button extends Component<Props> {
 
-  navigate = () => {this.props.navigation.navigate(this.props.goTo, {screen: this.props.screen});}
-
   render() {
 
     return (
-      <TouchableOpacity style={this.styles.button} onPress={this.navigate}>
+      <TouchableOpacity
+        style={this.props.disable ? this.styles.button__disable : this.styles.button}
+        onPress={this.props.disable ? () => { } : this.props.onPress}
+        >
         <Text style={this.styles.text}>{this.props.text}</Text>
         <FontAwesome5 style={{marginLeft: 15}} size={20} color={'#fff'} name={'arrow-right'} />
       </TouchableOpacity>
@@ -36,6 +36,17 @@ export default class Button extends Component<Props> {
       paddingHorizontal: 20,
       paddingVertical: 10,
       backgroundColor: '#FCB040',
+      borderRadius: 15,
+      borderTopRightRadius: 0,
+      alignSelf: 'flex-end'
+    },
+    button__disable: {
+      flexDirection: "row",
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      backgroundColor: '#E6E6E6',
       borderRadius: 15,
       borderTopRightRadius: 0,
       alignSelf: 'flex-end'
