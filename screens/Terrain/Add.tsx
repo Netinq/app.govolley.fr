357e083e127a6dc41c44deafe59eeb5792eb850d
BuildTools <sarquentin@gmail.com>
2022-06-16 03:20:40 +0200

    :sparkles: Adding quizz

--- screens/Terrain/Add.tsx
+++ screens/Terrain/Add.tsx
@@ -1,28 +1,45 @@
-import { Component, useState } from "react";
-import * as Store from 'expo-secure-store'
 import { QuizzTabParamList, RootStackParamList, TerrainTabScreenProps } from "../../types";
-import { CompositeNavigationProp } from "@react-navigation/native";
-import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
 import { Title } from "../../components/Texts/Title";
 import { Background } from "../../components/Background";
 import ChatBox from "../../components/Chat/ChatBox";
 import Chat from "../../components/Chat/Chat";
 import ButtonColor from "../../components/Chat/ButtonColor";
+import { Header } from "../../components/Header";
+
+import * as Store from 'expo-secure-store'
 
 export default function Add({ navigation }: TerrainTabScreenProps<'Add'>) {
+
+  const cancel = () => {
+    navigation.goBack()
+  }
+
+  const pressPublic = async () => {
+
+    const area = {
+      type: 'public'
+    }
+
+    await Store.setItemAsync('new_area', JSON.stringify(area))
+    next()
+  }
+
+  const next = () => {
+    navigation.navigate("Picture");
+  }
 
   return (
     <View style={styles.container}>
       <Background />
+      <Header onlyBack={true} backPress={cancel} customBack='annuler' />
       <Title title='Ajouter un terrain' style={{ marginTop: 25 }} big={true}></Title>
       <ChatBox style={{ marginTop: 25 }}>
         <Chat icon={true}>Tiens ! Je te revois enfin ? Pour ajouter un terrain c'est très simple.</Chat>
         <Chat>Veux-tu répertorier un terrain publique ou veux-tu signaler la présence de ton terrain ?</Chat>
         <View style={styles.buttonContainer}>
-          <ButtonColor text="Mon terrain" subText="(Ephémère)" color="#74C6F6" onPress={() => {}}/>
-          <ButtonColor text="Terrain publique" color="#F674C2" onPress={() => {}}/>
+          <ButtonColor text="Mon terrain" subText="(Ephémère)" color="#74C6F6" onPress={() => {}} disable={true} />
+          <ButtonColor text="Terrain publique" color="#F674C2" onPress={pressPublic}/>
         </View>
       </ChatBox>
     </View>
