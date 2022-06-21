04fc2373cfc0d052b26dba2b4248ba7df8addecd
BuildTools <sarquentin@gmail.com>
2022-06-21 05:03:03 +0200

    :sparkles: Adding no text children

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,8 +1,11 @@
 import { Component } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, ViewStyle } from "react-native";
 
 type Props = {
   icon?: boolean
+  reverse?: boolean,
+  style?: ViewStyle,
+  noText?: boolean
 }
 
 export default class Chat extends Component<Props> {
@@ -10,9 +13,14 @@ export default class Chat extends Component<Props> {
   render() {
 
     return (
-      <View style={this.styles.chat}>
+      <View style={this.props.reverse ? this.styles.chatReverse : [this.styles.chat, this.props.style]}>
         {this.props.icon && <Image style={this.styles.robot} source={require('../../assets/images/favicon.png')} />}
+        {
+          (!this.props.noText) ?
         <Text style={this.styles.text}>{this.props.children}</Text>
+        :
+        this.props.children
+        }
       </View>
     )
 
@@ -28,10 +36,19 @@ export default class Chat extends Component<Props> {
       width: '100%',
       marginBottom: 25
     },
+    chatReverse: {
+      paddingHorizontal: 35,
+      paddingVertical: 20,
+      backgroundColor: '#fff',
+      borderRadius: 15,
+      borderTopRightRadius: 0,
+      width: '100%',
+      marginBottom: 25
+    },
     text: {
       fontFamily: 'franklin-gothic-medium',
       fontSize: 18,
-      color: '#353535'
+      color: '#353535',
     },
     robot: {
       position: 'absolute',
